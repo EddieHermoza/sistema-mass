e7005a7d5c5c3cc3c4bea2badfe663188a6682f6
EddieHermoza <eddie.ehc04@gmail.com>
2024-09-07 14:17:59 -0500

    fix: sales page

--- src/app/admin/sales/page.tsx
+++ src/app/admin/sales/page.tsx
@@ -8,13 +8,10 @@ import {
     CardHeader,
     CardTitle,
 } from "@/components/ui/card"
-import { Suspense } from "react";
-import { Pagination } from "@/components/ui";
-import { IoAddCircleOutline } from "react-icons/io5";
-import ToogleStatus from "@/components/filters/toogle-status";
-import { Input } from "@/components/ui/input";
-import { IoSearchOutline } from "react-icons/io5";
 
+import { IoAddCircleOutline } from "react-icons/io5";
+import SearchByName from "@/components/filters/search-name";
+import ToogleLimit from "@/components/filters/toogle-limit";
 
 export default function Page({searchParams}:any) {
     const query = searchParams?.query || ''
@@ -26,13 +23,8 @@ export default function Page({searchParams}:any) {
         <>
             <div className="w-full flex items-end justify-between max-sm:flex-col-reverse gap-3">
                 <div className="space-y-2 max-sm:w-full">
-                    <label className="relative flex-center ">
-                        <IoSearchOutline className="absolute left-2.5 top-1/2 -translate-y-1/2 h-4 w-4 text-muted-foreground" />
-                        <Input
-                        placeholder="Busqueda"
-                        className="rounded-lg bg-background pl-8 w-full sm:w-96 duration-200"
-                        />
-                    </label>   
+                    <SearchByName/>
+                    <ToogleLimit/>
                     <ToogleStatus/>
                 </div>
 
@@ -42,14 +34,9 @@ export default function Page({searchParams}:any) {
                     <CardTitle>Ventas</CardTitle>
                     <CardDescription>Visualiza las ventas de la plataforma</CardDescription>
                 </CardHeader>
-                <CardContent>
-                    <InventoryTbl page={currentPage} limit={limit} query={query} status={status} />
-                </CardContent>
-                <CardFooter className="flex-center">
-                    <Suspense>
-                        <Pagination totalPages={11}/>
-                    </Suspense>
-                </CardFooter>
+                
+                <InventoryTbl page={currentPage} limit={limit} query={query} status={status} />
+
             </Card>
         </>
     );
