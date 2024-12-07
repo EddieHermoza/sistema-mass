063039220354922aaab2d998c21f8c8801421823
EddieHermoza <eddie.ehc04@gmail.com>
2024-12-07 11:04:25 -0500

    add close session button on mobile

--- src/app/(shop)/account/page.tsx
+++ src/app/(shop)/account/page.tsx
@@ -1,5 +1,6 @@
 import UserChangePasswordForm from "@/components/account/user-change-password-form";
 import UserDataForm from "@/components/account/user-data-form";
+import CloseSessionButton from "@/components/session/close-session-btn";
 import { Button } from "@/components/ui/button";
 import { Card, CardHeader, CardTitle, CardDescription, CardContent } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
@@ -24,6 +25,8 @@ export default async function Page() {
 
 
                 <UserChangePasswordForm id={id}/>
+
+                <CloseSessionButton label="Cerrar Sesión" iconSize={22} className="p-2 w-full md:hidden bg-primary hover:bg-primary/80"/>
 
             </div>
         </>
